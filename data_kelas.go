687a05f2915e2db02188d24685dc694cbdaf6925
Diva2504 <64818972+Diva2504@users.noreply.github.com>
2022-03-01 15:24:00 +0800

    mengubah data pada di data_kelas.go

--- data_kelas.go
+++ data_kelas.go
@@ -18,16 +18,16 @@ func (k kelas) tampil() {
 
 func main() {
 	argument := os.Args
-	faris := kelas{Nama: "1", Alamat: "1", Pekerjaan: "1", Alasan: "1"}
-	rakha := kelas{Nama: "2", Alamat: "2", Pekerjaan: "2", Alasan: "2"}
-	diva := kelas{Nama: "3", Alamat: "3", Pekerjaan: "3", Alasan: "3"}
-	daffa := kelas{Nama: "4", Alamat: "4", Pekerjaan: "4", Alasan: "4"}
-	yoga := kelas{Nama: "5", Alamat: "5", Pekerjaan: "5", Alasan: "5"}
-	azhar := kelas{Nama: "6", Alamat: "6", Pekerjaan: "6", Alasan: "6"}
-	haiqal := kelas{Nama: "7", Alamat: "7", Pekerjaan: "7", Alasan: "7"}
-	luqman := kelas{Nama: "8", Alamat: "8", Pekerjaan: "8", Alasan: "8"}
-	ruben := kelas{Nama: "9", Alamat: "9", Pekerjaan: "9", Alasan: "9"}
-	denada := kelas{Nama: "10", Alamat: "10", Pekerjaan: "10", Alasan: "10"}
+	faris := kelas{Nama: "Ahmad Faris", Alamat: "1", Pekerjaan: "1", Alasan: "1"}
+	rakha := kelas{Nama: "Rakha Rizqllah Pratama Saputra", Alamat: "2", Pekerjaan: "2", Alasan: "2"}
+	diva := kelas{Nama: "I Gede Diva Dwijayana", Alamat: "Karangasen, Bali", Pekerjaan: "Mahasiswa", Alasan: "karena penasaran dengan bahasa pemrograman Golang, ingin mempelajarinya, dan membuat sebuah aplikasi dari Golang"}
+	daffa := kelas{Nama: "Muhammad Daffa Haryadi Putra", Alamat: "4", Pekerjaan: "4", Alasan: "4"}
+	yoga := kelas{Nama: "M yoga irvandi", Alamat: "Sumatra Selatan", Pekerjaan: "Mahasiswa", Alasan: "karena Golang merupakan bahasa pemrograman masih tergolong baru,yg menarik untuk di pelajari, bagi saya."}
+	azhar := kelas{Nama: "Wafianda Azhar", Alamat: "6", Pekerjaan: "6", Alasan: "6"}
+	haiqal := kelas{Nama: "Muhamad Haiqal Malik", Alamat: "7", Pekerjaan: "7", Alasan: "7"}
+	luqman := kelas{Nama: "Luqman Fauzi", Alamat: "8", Pekerjaan: "8", Alasan: "8"}
+	ruben := kelas{Nama: "Rubenhard Manat Hasiholan Lumbantobing", Alamat: "9", Pekerjaan: "9", Alasan: "9"}
+	denada := kelas{Nama: "Denada Ramschie", Alamat: "10", Pekerjaan: "10", Alasan: "10"}
 
 	if len(argument) == 2 {
 		switch argument[1] {
